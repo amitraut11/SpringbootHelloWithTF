9ccc8a4f7fed2d1ebe09e5fd28929eff935d3f7b
amit.raut <amitraut11@gmail.com>
2024-03-14 09:23:53 -0500

    added new files

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "useeffectbackend"
+    bucket  = "springboothelloworldbackend"
     encrypt = true
     key     = "terraform.tfstate"
     region  = "us-east-1"
